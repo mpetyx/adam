f3ece38abf3d701f95aae1723df110e3b28b2348
Michael Petychakis <mpetyx@gmail.com>
2014-01-08 22:43:27 +0200

    deeper query implementation

---
+++ SWQueries/european_countries.py
@@ -0,0 +1,26 @@
+__author__ = 'mpetyx'
+
+def query():
+
+    query = """
+
+    PREFIX dbpedia-owl:<http://dbpedia.org/ontology/>
+    PREFIX category: <http://dbpedia.org/resource/Category:>
+    PREFIX dcterms:<http://purl.org/dc/terms/>
+    PREFIX prop:<http://dbpedia.org/property/>
+    PREFIX geonames:<http://sws.geonames.org/>
+    PREFIX rdfs:<http://www.w3.org/2000/01/rdf-schema#>
+    PREFIX dbpedia:<http://dbpedia.org/resource/>
+    PREFIX geo-ont:<http://www.geonames.org/ontology#>
+    PREFIX ourvocab:<http://example.org/>
+
+    SELECT  COUNT(*)
+    WHERE {
+
+
+    ?european_country dcterms:subject category:Countries_in_Europe.
+    ?european_country rdf:type dbpedia-owl:Place.
+
+    }
+    LIMIT 200
+"""
--- SWQueries/painters.py
+++ SWQueries/painters.py
@@ -1,6 +1,9 @@
 __author__ = 'mpetyx'
 
 def query():
+    """
+    this query searches for oil painter artists for around the europe
+    """
 
     superQuery = """
 
@@ -14,24 +17,30 @@ def query():
     PREFIX geo-ont:<http://www.geonames.org/ontology#>
     PREFIX ourvocab:<http://example.org/>
 
-    SELECT DISTINCT ?artist_name ?painting_name ?european_country
+    SELECT DISTINCT ?artist_name ?painting_name ?european_country ?artist_birthday_year
     WHERE {
 
     ?artist prop:name ?artist_name.
     ?artist rdf:type dbpedia-owl:Artist.
     ?artist prop:placeOfBirth ?european_country.
+    ?artist dbpedia-owl:birthDate ?artist_birthday.
 
     ?painting prop:artist ?artist.
     ?painting rdfs:label ?painting_name.
+    ?painting prop:type dbpedia:Oil_painting.
 
     ?european_country dcterms:subject category:Countries_in_Europe.
     ?european_country rdf:type dbpedia-owl:Place.
 
 
-    } LIMIT 200
+     FILTER (lang(?painting_name) = "" || lang(?painting_name) = "en")
+     BIND(year(?artist_birthday) AS ?artist_birthday_year)
 
-
-
+    }
+GROUP BY ?painting_name
+LIMIT 200
 
 
     """
+
+    return superQuery
