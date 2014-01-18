d442d857dfed73682ed26cd97a1a1ad1dcf9c40a
Vaggelis Kostas <underworldseed@gmail.com>
2014-01-18 06:04:21 +0200

    NN stuff

--- SWQueries/museum_data/settings.py
+++
@@ -1,5 +0,0 @@
-__author__ = 'mpetyx'
-
-EUROPEANA = "http://europeana.ontotext.com/sparql"
-
-BRITISH_MUSEUM = "http://collection.britishmuseum.org/sparql"
--- SWQueries/music/settings.py
+++
@@ -1,5 +0,0 @@
-__author__ = 'mpetyx'
-
-DARWINS_MUSIC = "http://83.212.105.61:10035/catalogs/system/repositories/darwinsmusic"
-
-original_repository = "https://github.com/mpetyx/DarwinsMusic"
